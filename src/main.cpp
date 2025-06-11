0b457f04f990923f89be5efe4dffbc4186118a1f
chocolateimage <chocolateimage@protonmail.com>
2025-06-11 20:23:59 +0200

    Add German translation

--- src/main.cpp
+++ src/main.cpp
@@ -692,6 +692,11 @@ MainWindow::~MainWindow() {
 }
 
 int main(int argc, char** argv) {
+    setlocale(LC_ALL, "");
+    bindtextdomain(GETTEXT_PACKAGE, DATADIR "/locale");
+    bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+    textdomain(GETTEXT_PACKAGE);
+
     bool entryGoogleFontsValue = false;
 
     Glib::OptionEntry entryGoogleFonts;
